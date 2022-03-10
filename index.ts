49a67ffa5548adeb31134b868b9147f45a41ce26
BEAUTAR <83558631+BEAUTAR@users.noreply.github.com>
2022-03-10 09:00:42 +0300

    add: whitelist permission

--- index.ts
+++ index.ts
@@ -34,56 +34,60 @@ client.on('messageCreate', (message) => {
     const command = message.content.split(' ');
     if(message.author.id != client.user?.id) {
         if (command[0] == '/whitelist') {
-            if (command.length <= 1) {
-                const commandEmbed = new MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle('Enter your credentials')
-                .setAuthor('Credentials','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
-                .setDescription(`<@${message.author.id}> Please Enter Your email and wallet address e.g. /whitelist user@memail.com 0x4523g35gg35g53`)
-                .setImage('https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
-                //.addField('Inline field title', 'Some value here', true)
-                .setTimestamp(new Date())
-                .setFooter('Kindly ensure your credentials are in correct format',)
-                message.channel.send({
-                    embeds:[commandEmbed]
-                })
-            } else {
-                if (ValidateEmail(command[1])) {
-                    setValue({
-                        discordUsername: message.author.username,
-                        email: command[1],
-                        walletAddress: command[2]
-                    }, message.author.id)
-                    const commandEmbed = new MessageEmbed()
-                .setColor('RANDOM')
-                .setTitle('Thank You!')
-                .setAuthor('Credentials','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
-                .setDescription(`<@${message.author.id}> Your Address: ${command[2]} and email: ${command[1]} has been saved successfully.`)
-                .setImage('https://cdn.discordapp.com/attachments/908853989834129501/951176211000881162/Great-Job-Emoji-PNG-Photos.png')
-                //.addField('Inline field title', 'Some value here', true)
-                .setTimestamp(new Date())
-                .setFooter('Thank you for your time',)
-                message.author.send({
-                    embeds:[commandEmbed]
-                })
-                message.channel.send(`<@${message.author.id}> Check Your DM`);
-                } else {
+            if (message.member?.roles.cache.find(role => role.name.toLowerCase() == 'whitelist')) {
+                if (command.length <= 1) {
                     const commandEmbed = new MessageEmbed()
                     .setColor('RANDOM')
-                    .setTitle(`<@${message.author.id}> Please enter a Valid Email!`)
-                    .setAuthor('ERROR!','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
-                    //.setDescription(``)
-                    .setImage('https://cdn.discordapp.com/attachments/908853989834129501/951176835889266718/red-error-round-icon-2.png')
+                    .setTitle('Enter your credentials')
+                    .setAuthor('Credentials','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
+                    .setDescription(`<@${message.author.id}> Please Enter Your email and wallet address e.g. /whitelist user@memail.com 0x4523g35gg35g53`)
+                    .setImage('https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
                     //.addField('Inline field title', 'Some value here', true)
                     .setTimestamp(new Date())
-                    .setFooter('KIndly check your email format!',)
+                    .setFooter('Kindly ensure your credentials are in correct format',)
+                    message.channel.send({
+                        embeds:[commandEmbed]
+                    })
+                } else {
+                    if (ValidateEmail(command[1])) {
+                        setValue({
+                            discordUsername: message.author.username,
+                            email: command[1],
+                            walletAddress: command[2]
+                        }, message.author.id)
+                        const commandEmbed = new MessageEmbed()
+                    .setColor('RANDOM')
+                    .setTitle('Thank You!')
+                    .setAuthor('Credentials','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
+                    .setDescription(`<@${message.author.id}> Your Address: ${command[2]} and email: ${command[1]} has been saved successfully.`)
+                    .setImage('https://cdn.discordapp.com/attachments/908853989834129501/951176211000881162/Great-Job-Emoji-PNG-Photos.png')
+                    //.addField('Inline field title', 'Some value here', true)
+                    .setTimestamp(new Date())
+                    .setFooter('Thank you for your time',)
                     message.author.send({
                         embeds:[commandEmbed]
                     })
                     message.channel.send(`<@${message.author.id}> Check Your DM`);
+                    } else {
+                        const commandEmbed = new MessageEmbed()
+                        .setColor('RANDOM')
+                        .setTitle(`<@${message.author.id}> Please enter a Valid Email!`)
+                        .setAuthor('ERROR!','https://cdn.discordapp.com/attachments/909881951639437392/951144533670903849/Attachment_1646840820-1.jpeg')
+                        //.setDescription(``)
+                        .setImage('https://cdn.discordapp.com/attachments/908853989834129501/951176835889266718/red-error-round-icon-2.png')
+                        //.addField('Inline field title', 'Some value here', true)
+                        .setTimestamp(new Date())
+                        .setFooter('KIndly check your email format!',)
+                        message.author.send({
+                            embeds:[commandEmbed]
+                        })
+                        message.channel.send(`<@${message.author.id}> Check Your DM`);
+                    }
                 }
+                message.delete();
+            } else {
+                message.channel.send(`<@${message.author.id}> you do not have the Whitelist Role.`);
             }
-            message.delete();
         } else if(command[0] == '/help') {
             const commandEmbed = new MessageEmbed()
                     .setColor('RANDOM')
@@ -97,6 +101,8 @@ client.on('messageCreate', (message) => {
                     message.channel.send({
                         embeds:[commandEmbed]
                     })
+        } else if(command[0] == '/test') {
+            console.log('test');
         }
     }
 })
